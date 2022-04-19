612928593251f29d0fb09091079efbff3d65bb90
Nathan Smith <nsmith@lcs.on.ca>
2022-04-19 19:13:54 -0400

    done

--- FizzBuzzGameSolution.playground/Pages/RSA Numbers.xcplaygroundpage/Contents.swift
+++ FizzBuzzGameSolution.playground/Pages/RSA Numbers.xcplaygroundpage/Contents.swift
@@ -3,16 +3,19 @@
 import Foundation
 
 let lowerBound = 1
-let upperBound = 20
+let upperBound = 150
 var howManyRSANumbers = 0
 
 for n in lowerBound...upperBound {
     var divisors = 0
     for j in 1...n {
         if n.isMultiple(of: j) {
-           divisors += 1
+            divisors += 1
         }
     }
+    if divisors == 4 {
+        howManyRSANumbers += 1
+    }
 }
-
+print(howManyRSANumbers)
 //: [Next](@next)
